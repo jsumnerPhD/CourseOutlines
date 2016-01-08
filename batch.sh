161cf8d6d915086f5acd2c143baddf47e6297894
jfbriere <jfbriere@dawsoncollege.qc.ca>
2016-01-08 17:08:32 -0500

    912 and copying files

--- batch.sh
+++ batch.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
+
 session="_Winter2016"
+
 # Batch file to generate course outlines for all directories starting 203
-for dir in *912*;do 
+
+for dir in *912*;do
 echo 'Generating outline for' $dir
+
 # Copy template.tex
 cp ./template.tex ./$dir/$dir.tex
+
 # Copy dawsoncourseoutline.cls
 cp ./dawsoncourseoutline.cls ./$dir/dawsoncourseoutline.cls
+
 # Copy logo
 cp ./dawson.png ./$dir/dawson.png
+
 cd $dir
 pdflatex $dir.tex > pdflatex.log
 mv $dir.pdf $dir$session.pdf
